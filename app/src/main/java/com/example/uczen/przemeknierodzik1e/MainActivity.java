2123beee40764e9cbbc311c782ab00731454910c
przemek <Jackg7895@gmail.com>
2017-11-09 21:03:20 +0100

    Startup with some shit

--- app/src/main/java/com/example/uczen/przemeknierodzik1e/MainActivity.java
+++ app/src/main/java/com/example/uczen/przemeknierodzik1e/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        p1 = (button) findViewById("@+id/button");
+        p1.setOnClick
     }
 }
